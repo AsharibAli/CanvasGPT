174b47ae4338f553c6272b0fcae422b226bde5cb
Asharib Ali <asharibarain4@gmail.com>
2024-08-25 11:20:14 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -8,18 +8,18 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-                default-src 'self';
-                script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net;
-                style-src 'self' 'unsafe-inline';
-                img-src 'self' https://raw.githubusercontent.com;
-                connect-src 'self' ${process.env.NEXT_PUBLIC_FLOWISE_API_HOST} ${process.env.NEXT_PUBLIC_WEBSOCKET_URL};
-                media-src 'self' https://cdn.jsdelivr.net;
-                frame-ancestors 'self' https://dscvr.one;
-                object-src 'none';
-                base-uri 'self';
-              `
-              .replace(/\s{2,}/g, " ")
-              .trim(), // Remove excess whitespace and trim
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' https://raw.githubusercontent.com;
+              connect-src 'self' ${process.env.NEXT_PUBLIC_FLOWISE_API_HOST} ${process.env.NEXT_PUBLIC_WEBSOCKET_URL} https://api.devnet.solana.com wss://api.devnet.solana.com;
+              media-src 'self' https://cdn.jsdelivr.net;
+              frame-ancestors 'self' https://dscvr.one;
+              object-src 'none';
+              base-uri 'self';
+            `
+            .replace(/\s{2,}/g, " ")
+            .trim(), // Remove excess whitespace and trim
           },
         ],
       },
